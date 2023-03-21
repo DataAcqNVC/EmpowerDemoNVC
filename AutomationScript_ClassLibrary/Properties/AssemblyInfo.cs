a30867508304074203c282ce4ae107eb9b1074e4
Nicolas Vercaigne <nicolas.vercaigne@skyline.be>
2023-03-21 15:02:02 +0100

    Add project files.

---
+++ AutomationScript_ClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationScript_ClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AutomationScript_ClassLibrary")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4957E5F6-9EC2-4728-BA19-E6ADD0FF72A4")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Empower_NVC_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_NVC_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_NVC_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("42260614-9FC8-45B0-85ED-8E6656C57636")]
+[assembly: AssemblyVersion("1.0.0.0")]
